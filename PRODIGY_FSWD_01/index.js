90e06b9e2bb554af62872fbe0db1845cfee6d687
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-01-10 20:17:00 +0530

    Writing first project starting part

---
+++ PRODIGY_FSWD_01/index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+// Direct MongoDB connection string
+const MONGO_URL = "mongodb://127.0.0.1:27017/Study_Space";
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Define the user schema and model
+const userSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// Routes
+app.get("/login", (req, res) => {
+  res.render("listings/login");
+});
+
+// app.get("/signup", (req, res) => {
+//   res.render("listings/signup.ejs");
+// });
+
+// Register User
+app.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ name: username });
+
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: username,
+      password: hashedPassword,
+      email: email,
+    });
+
+    await newUser.save();
+    res.status(201).render("listings/login");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Login user
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ name: username });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.status(200).render("listings/index.ejs");
+    } else {
+      res.status(400).send("Wrong Password");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
