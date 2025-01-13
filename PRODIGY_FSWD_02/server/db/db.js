1b45ccc25e5e9a24cd2f76024d48f86a62c109b8
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-01-13 09:28:53 +0530

    Constructing Employee management System

---
+++ PRODIGY_FSWD_02/server/db/db.js
@@ -0,0 +1,27 @@
+// import mongoose from "mongoose";
+
+// const connectToDatabase = async()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URL)
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+
+// export default connectToDatabase;
+
+
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Connected to MongoDB successfully!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1); // Exit the process if the connection fails
+    }
+};
+
+export default connectToDatabase;
+
