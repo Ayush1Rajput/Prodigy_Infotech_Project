89d062c5d1f20a63c7d51f29cd9d941f79a015bb
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-01-25 22:41:29 +0530

    Adding models DBmodel or Scheama for DB

--- PRODIGY_FSWD_03/server/index.js
+++ PRODIGY_FSWD_03/server/index.js
@@ -1,31 +1,44 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import morgan from "morgan"
-import helmet from "helmet"
-
-
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
 
 const app = express()
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL
 }))
-
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan())
-app.use(helmet(
-    {
-        crossOriginResourcePolicy: false
-    }
-))
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}))
 
+const PORT = 8080;
 
-const PORT = 5050;
-app.listen(PORT, () => {
-    console.log("Server is running on " + PORT)
+app.get("/", (request, response) => {
+    ///server to client
+    response.json({
+        message: "Server is running " + PORT
+    })
+})
+
+// app.use('/api/user', userRouter)
+// app.use("/api/category", categoryRouter)
+// app.use("/api/file", uploadRouter)
+// app.use("/api/subcategory", subCategoryRouter)
+// app.use("/api/product", productRouter)
+// app.use("/api/cart", cartRouter)
+// app.use("/api/address", addressRouter)
+// app.use('/api/order', orderRouter)
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running", PORT)
+    })
 })
